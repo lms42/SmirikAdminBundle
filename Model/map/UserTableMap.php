177e684fd17c4d49ea3f858f72b75c32fad5fb6f
Evgeny Smirnov <smirik@gmail.com>
2012-09-04 23:18:45 +0400

    Add propel generated class to gitignore

--- Model/map/UserTableMap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Smirik\AdminBundle\Model\map;
-
-use FOS\UserBundle\Propel\map\UserTableMap as BaseUserTableMap;
-
-class UserTableMap extends BaseUserTableMap
-{
-} // UserTableMap

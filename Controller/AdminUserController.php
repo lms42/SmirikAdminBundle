6ae0bb748f07a338ed78b337d99d997620ef629f
Evgeny Smirnov <smirik@gmail.com>
2013-12-17 08:28:36 +0400

    Add logs

--- Controller/AdminUserController.php
+++ Controller/AdminUserController.php
@@ -20,18 +20,32 @@ class AdminUserController extends BaseController
 	 */
 	public function logsAction($id)
 	{
-	    $um = $this->get('fos_user.user_manager');
-	    $user = $um->findUserBy(array('Id' => $id));
-
-        $now = new \DateTime();
-        $month_ago = clone($now);
-        $month_ago = $month_ago->modify('-1 month');
+	    $user = $this->get('fos_user.user_manager')->findUserBy(array('Id' => $id));
         
-		$logs = $this->get('lms42.log.manager')->getLogsByDate($user, $now, $month_ago);
-        var_dump('stop');
-        exit();
+        $start = $this->getRequest()->query->get('start', false);
+        $stop  = $this->getRequest()->query->get('stop', false);
         
-		return $response;
+        if ($start) {
+            $start = new \DateTime($start);
+        } else {
+            $start = new \DateTime();
+            $start->modify('-1 week');
+        }
+        
+        if ($stop) {
+            $stop = new \DateTime($stop);
+        } else {
+            $stop = new \DateTime();
+        }
+        
+		$logs = $this->get('lms42.log.manager')->getUserLogs($user, $start, $stop);
+        
+        return array(
+            'user'  => $user,
+            'logs'  => $logs,
+            'start' => $start,
+            'stop'  => $stop,
+        );
 	}
     
 	/**
